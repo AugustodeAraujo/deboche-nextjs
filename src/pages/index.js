cd4c434e1d8078f2f93af10e488176b3a70827eb
AugustodeAraujo <poxaugustodantas@gmail.com>
2021-08-12 03:20:32 -0300

    Animated Logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Flex, Fade, ScaleFade, Slide, SlideFade,
+} from '@chakra-ui/react';
 import Image from 'next/image';
+import { useState } from 'react';
 import logo from '../../public/logo.png';
 
 export default function Home() {
+  // eslint-disable-next-line no-unused-vars
+  const [animation, setAnimation] = useState(true);
+
   return (
+
     <Flex
       w="100vw"
       h="100vh"
@@ -12,7 +19,9 @@ export default function Home() {
       justify="center"
     >
       <Flex direction="column" align="center">
-        <Image src={logo} width="270" height="270" />
+        <ScaleFade initialScale={0} in={setAnimation}>
+          <Image src={logo} width="270" height="270" />
+        </ScaleFade>
       </Flex>
     </Flex>
   );
